6b5d738a88884a4ccfaf31fd09d763603075a020
daria <armi.din1902@gmail.com>
2023-06-15 20:57:34 +0300

    feat: add timeout to python matcher

--- python_re/matcher.py
+++ python_re/matcher.py
@@ -1,14 +1,28 @@
 import re
 import time
 import sys
+import signal
 from matplotlib import pyplot as plt
+
+class TimeoutException(Exception):
+    pass
+
+def timeout_handler(signum, frame):
+    raise TimeoutException("Function timed out")
 
 def match_timer(r, input_str):
     regexp = re.compile("^" + r + "$")
-    start_time = time.perf_counter()
-    result = regexp.match(input_str)
-    execute_time = time.perf_counter() - start_time
-    return execute_time
+    signal.signal(signal.SIGALRM, timeout_handler)
+    signal.alarm(1)
+
+    try:
+        start_time = time.perf_counter()
+        result = regexp.match(input_str)
+        return time.perf_counter() - start_time
+    except TimeoutException:
+        return 1
+    finally:
+        signal.alarm(0)
 
 example_number = sys.argv[1]
 xs = []
@@ -22,10 +36,15 @@ with open(f'../test/example_{example_number}/regexp.txt', 'r') as file, \
     (mfa_r, r) = text.split('\n')
     print(r)
     strings = input_file.read().split('\n')
+    execute_time = 0
     for string in strings:
         if string != '':
             xs.append(len(string))
-            res_file.write(str(match_timer(r, string)) + '\n')
+            if execute_time < 1:
+                execute_time = match_timer(r, string)
+            if execute_time < 1:
+                res_file.write(str(execute_time) + '\n')
+
 
 with open(f'../test/example_{example_number}/python_results.txt', 'r') as res_file1, \
         open(f'../test/example_{example_number}/diploma_results.txt', 'r') as res_file2, \
@@ -48,19 +67,19 @@ with open(f'../test/example_{example_number}/python_results.txt', 'r') as res_fi
     axs = axs.flatten()
 
 
-    axs[0].plot(xs, ys2)
+    axs[0].plot(xs[:len(ys2)], ys2)
     axs[0].set(xlabel='Length, chars', ylabel='Time, seconds')
     axs[0].set_title("diploma")
     if len(ys3) > 0:
-        axs[1].plot(xs, ys3)
+        axs[1].plot(xs[:len(ys3)], ys3)
         axs[1].set(xlabel='Length, chars', ylabel='Time, seconds')
         axs[1].set_title("diploma reverse")
 
-        axs[2].plot(xs, ys1)
+        axs[2].plot(xs[:len(ys1)], ys1)
         axs[2].set(xlabel='Length, chars', ylabel='Time, seconds')
         axs[2].set_title("python")
     else:
-        axs[1].plot(xs, ys1)
+        axs[1].plot(xs[:len(ys1)], ys1)
         axs[1].set(xlabel='Length, chars', ylabel='Time, seconds')
         axs[1].set_title("python")
 
